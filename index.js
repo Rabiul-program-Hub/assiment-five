559168eb02e39f6e3a3bec01312edaabf4c0a606
Rabiul-devlop-hub <rabiulawalcoder@gmail.com>
2025-01-17 00:24:34 +0600

    all section desing is ok

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('all right')
+const continueBtn = document.getElementById('continue-btn')
+continueBtn.addEventListener('click',function(){
+  console.log('clicked item')
+})
+console.log(continueBtn)
